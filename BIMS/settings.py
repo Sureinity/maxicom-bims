c9d16d1dc5c93853a745440130af3737a96bea87
Sureinity <johnghlendealdo@gmail.com>
2026-01-06 15:17:12 +0800

    refactor(settings): Align environment keys with PostgreSQL Docker image

--- BIMS/settings.py
+++ BIMS/settings.py
@@ -92,11 +92,11 @@ WSGI_APPLICATION = 'BIMS.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('PASSWORD'),
-        'HOST': config('HOST'),
-        'PORT': config('PORT'),
+        'NAME': config('POSTGRES_DB'),
+        'USER': config('POSTGRES_USER'),
+        'PASSWORD': config('POSTGRES_PASSWORD'),
+        'HOST': config('POSTGRES_HOST'),
+        'PORT': config('POSTGRES_PORT'),
 
     }   
 }
